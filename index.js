7acc0cb8fcd5e5e18f2fe358506ea531b98bab9e
henok Gebremedhn <henoklwi@gmail.com>
2020-04-23 02:48:01 -0700

    worked on todo list

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-// TODO: What is this line doing?
+// DONE: What is this line doing?
+//This line will allow us to use all the
+// files in the environmental variables.
 require('dotenv').config();
 
-// TODO: What is this line doing?
+// DONE: What is this line doing?
+//This line is importing our server and starting our
+// server by using the stored environmental variables in the port.
 require('./lib/server.js').start(process.env.PORT);
